ca85f25837690ff4f76edf3c0336e8393e18d6eb
luohechen <luoheqp2020@gmail.com>
2021-12-23 15:12:36 +0800

    :wrench:(the-star): add postinstall

---
+++ packages/the-star/postinstall.js
@@ -0,0 +1,7 @@
+var BANNER =
+  '\n' +
+  '\u001B[96mThe-Star using sass as pre-processor\u001B[0m\n' +
+  '\u001B[96mmake sure project installed sass or node-sass\u001B[0m\n' +
+  '\n';
+
+console.log(BANNER);
