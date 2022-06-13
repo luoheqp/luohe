e4c2cc12efe74511fe2b0c252012f3793933c711
luohechen <luoheqp2020@gmail.com>
2022-06-13 17:07:45 +0800

    :wrench:(lint): config change

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,12 +1,4 @@
-const { executor } = require('../utils/preCommit.mjs');
-
 module.exports = {
-  '*': [
-    (files) => {
-      executor(files);
-    },
-    'git add .',
-  ],
   '*.{js,vue,jsx,tsx,mjs}': ['eslint --fix'],
   '*.{scss,sass,css,less}': ['prettier --write'],
 };
